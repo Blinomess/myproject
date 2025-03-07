d19265f5757ed81a449d4e067c753839c114e227
Blinomess <kolybudinov@gmail.com>
2025-03-07 05:47:16 +0300

    bls bla

--- message/views.py
+++ message/views.py
@@ -1,21 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import MessagesForm
 
 def message_text(request):
-    error=''
     if request.method=='POST':
         form=MessagesForm(request.POST)
         if form.is_valid():
             form.save()
-            #добавить высвечивание сообщения о успешной отправке сообщения или см в.9 21:00
-        else:
-            error='Сообщение было написано неверно'
+            return redirect('message_complete')
+    else:   
+        form = MessagesForm()
 
-    form= MessagesForm()
+    return render(request, 'message\make_a_message.html', {'form': form})
 
-    data={
-        'form': form,
-        'error': error
-    }
-
-    return render(request, 'message/make_a_message.html',data)
+def message_complete(request):
+    return render(request, 'message\message_complete.html')
