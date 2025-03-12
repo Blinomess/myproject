f5f984d5c1abcefcc45097c4affeb32374412bd6
Blinomess <kolybudinov@gmail.com>
2025-03-12 21:13:17 +0300

    final stage

--- message/models.py
+++ message/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
 class Messages(models.Model):
+    MES_STATUS=[('new', 'Новое'),
+                ('read', 'Прочитано')
+                 ]
     title=models.CharField('Тема', max_length=50)
     mes_text=models.TextField('Сообщение')
     ent_mail=models.CharField('Почта',max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
+    status=models.CharField(max_length=5, choices=MES_STATUS, default='new')
     def __str__(self):
         return self.title
     class Meta:
         verbose_name='Сообщение'
         verbose_name_plural='Сообщения'
+
+class Location(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
--- message/views.py
+++ message/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import MessagesForm
+from .models import Location
 
 def message_text(request):
     if request.method=='POST':
@@ -14,3 +15,12 @@ def message_text(request):
 
 def message_complete(request):
     return render(request, 'message\message_complete.html')
+
+def map_view(request):
+    if request.method == "POST":
+        lat = request.POST.get("latitude")
+        lng = request.POST.get("longitude")
+        if lat and lng:
+            Location.objects.create(latitude=lat, longitude=lng)
+            return redirect("success_page")  # Перенаправляем на страницу успеха
+    return render(request, "map.html")
